8fc796b7562dde9e2c849ce00f1d5f9b052acab3
Daniel Farrapo <danfarrapo@hotmail.com.br>
2022-11-24 13:55:08 -0300

    Adicionado mensagens a cada numeros multiplos de 5

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var counterVal = 0;
+//var counter5 = 5;
+var Patience = "Parabéns pela Paciência";
+var foco = "Foco!";
 
 function incrementClick() {
-    updateDisplay(++counterVal);
+    updateDisplay(++counterVal), patience();
 }
 
 function decrementClick() {
-    updateDisplay(--counterVal);
+    updateDisplay(--counterVal), patience();
 }
 
 function resetCounter() {
@@ -16,5 +19,22 @@ function resetCounter() {
 function updateDisplay(val) {
     document.getElementById("counter-label").innerHTML = val;
     
-}const audio = new Audio('WHISTLE.WAV');
+}
+
+const audio = new Audio('WHISTLE.WAV');
 audio.play();
+
+
+function patience() {
+  // if (counterVal == counter5) {updatepatience()}
+    if (counterVal%5 == 0) {updatepatience()}
+       else {updatepatience2()}
+}
+
+function updatepatience() {
+    document.getElementById("counter-patience").innerHTML = Patience;
+    }
+
+function updatepatience2() {
+    document.getElementById("counter-patience").innerHTML = foco;
+    }
